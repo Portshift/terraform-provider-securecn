91d35e86b509bb72eacf66ed8acbb96554e9e989
Nándor István Krácser <bonifaido@gmail.com>
2022-03-03 14:11:51 +0100

    install kubectl on-demand

--- securecn/provider.go
+++ securecn/provider.go
@@ -2,12 +2,18 @@ package securecn
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+	"terraform-provider-securecn/internal/client"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"log"
-	"os"
-	"terraform-provider-securecn/internal/client"
 )
 
 const ClusterResourceName = "securecn_k8s_cluster"
@@ -21,14 +27,69 @@ const AccessKeyFieldName = "access_key"
 const SecretKeyFieldName = "secret_key"
 const ServerUrlFieldName = "server_url"
 
+func downloadFile(filepath string, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
 func configureProviderClient(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
 	httpClient := client.NewHttpClient(
 		d.Get(AccessKeyFieldName).(string),
 		d.Get(SecretKeyFieldName).(string),
 		d.Get(ServerUrlFieldName).(string))
 
+	err := installKubectlOnDemand()
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+
 	log.Print("[DEBUG] httpClient created successfully")
 	return httpClient, nil
+}
+
+func installKubectlOnDemand() error {
+	kubectlPath, err := exec.LookPath("kubectl")
+	if err != nil {
+		kubectlDir := "/tmp/terraformbin/"
+		kubectlPath := kubectlDir + "kubectl"
+
+		_, err := os.Stat(kubectlPath)
+		if err != nil {
+			err = os.MkdirAll(kubectlDir, 0755)
+			if err != nil {
+				return err
+			}
+
+			kubectlURL := fmt.Sprintf("https://dl.k8s.io/release/v1.23.0/bin/%s/%s/kubectl", runtime.GOOS, runtime.GOARCH)
+			err = downloadFile(kubectlPath, kubectlURL)
+			if err != nil {
+				return err
+			}
+
+			log.Printf("[DEBUG] kubectl got downloaded to %s", kubectlPath)
+		} else {
+			log.Printf("[DEBUG] kubectl is available at %s", kubectlPath)
+		}
+
+		_ = os.Setenv("PATH", kubectlDir+":"+os.Getenv("PATH"))
+
+	} else {
+		log.Printf("[DEBUG] kubectl is available at %s", kubectlPath)
+	}
+
+	return nil
 }
 
 func Provider() plugin.ProviderFunc {
